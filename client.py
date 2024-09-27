ba6f78524f981476a05e5cc953f032efb8bac97b
Christian Gabriel Lara Lopez <christianlaracs@gmail.com>
2024-09-27 00:35:39 -0400

    Refactoring the code

--- client.py
+++ client.py
@@ -1,40 +1,79 @@
 import socket
 import json
 
+PUERTO = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PUERTO)
+HEADER = 128
+FORMATO = 'utf-8'
+JUEGO_LISTO = "JUEGO-CREADO"
+JUEGO_GANADO = "GANASTE"
+DESCONECTAR_MENSAJE = "Desconectar!" 
+
 # Para generar todos los numeros de 2 digitos que solo contienen 1,2 y 3
 def generar_combinaciones():
     return [f"{num1}{num2}" for num1 in range(1, 4) for num2 in range(1, 4)]
 
 # Funcion auxiliar para dar formato a la respuesta
 def procesar_pista(respuesta):
-    toros = respuesta["pistas"]["Toro"]
-    vacas = respuesta["pistas"]["Vaca"]
+    toros = respuesta["contenido"]["Toro"]
+    vacas = respuesta["contenido"]["Vaca"]
     return f"Toro: {toros}, Vaca: {vacas}"
+
+# Funcion auxiliar para devolver un string en formato json
+def crear_json(tipo, contenido):
+    diccionario = {"tipo": tipo, "contenido": contenido}
+    return json.dumps(diccionario)
+
+# Funcion auxiliar par enviar mensajes al servidor
+def enviar(msg):
+    mensaje = msg.encode(FORMATO)
+    longitud_mensaje = len(mensaje)
+    longitud_envio = str(longitud_mensaje).encode(FORMATO)
+    longitud_envio += b' ' * (HEADER - len(longitud_envio))
+    cliente_socket.send(longitud_envio)
+    cliente_socket.send(mensaje)
+
+# Funcion auxiliar para recibir mesajes del servidor
+def recibir():
+    mensaje = {}
+
+    longitud_mensaje = cliente_socket.recv(HEADER).decode(FORMATO)
+    if longitud_mensaje:
+        longitud_mensaje = int(longitud_mensaje)
+        mensaje = json.loads(cliente_socket.recv(longitud_mensaje).decode(FORMATO))
+    
+    return mensaje
+
+
 
 # Crear socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente_socket:
-    cliente_socket.connect(('localhost', 12345))
+    cliente_socket.connect(ADDR)
 
     print("Conectando al servidor...")
 
-    mensaje_inicial = json.loads(cliente_socket.recv(1024).decode())
-    
+    mensaje_inicial = recibir()
     if mensaje_inicial:
-        print(mensaje_inicial["contenido"])
+        if mensaje_inicial["contenido"] == JUEGO_LISTO:
+            print("Ya el juego esta listo!")
 
-        combinaciones = generar_combinaciones()
-            
-        for combinacion in combinaciones:
-            intento = {"tipo": "proposicion", "contenido": combinacion}
-            cliente_socket.send(json.dumps(intento).encode())
-            
-            respuesta = json.loads(cliente_socket.recv(1024).decode())
-            
-            if respuesta["tipo"] == "pista":
-                pista = procesar_pista(respuesta)
-                print(pista)
+            combinaciones = generar_combinaciones()
                 
-            elif respuesta["tipo"] == "estado":
-                if respuesta["contenido"] == "GANASTE":
-                    print("Felicidades, has ganado!!!")
-                    break
+            for combinacion in combinaciones:
+                intento = crear_json("proposicion", combinacion)
+                enviar(intento)
+                
+                respuesta = recibir()
+                
+                if respuesta["tipo"] == "pista":
+                    pista = procesar_pista(respuesta)
+                    print(pista)
+                    
+                elif respuesta["tipo"] == "estado":
+                    if respuesta["contenido"] == JUEGO_GANADO:
+                        print("Felicidades, has ganado!!!")
+                        enviar(crear_json("estado", DESCONECTAR_MENSAJE))
+                        break
+    else:
+        print("Error en el servidor. Terminando sesion...")
--- server.py
+++ server.py
@@ -2,6 +2,15 @@ import socket
 import random
 import json
 
+PUERTO = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PUERTO)
+HEADER = 128
+FORMATO = 'utf-8'
+JUEGO_LISTO = "JUEGO-CREADO"
+JUEGO_GANADO = "GANASTE"
+DESCONECTAR_MENSAJE = "Desconectar!"
+
 # Genera numero aleatorio de 2 cifras que solo contenga 1,2 y 3 
 def generar_numero_secreto():
     unidades = random.randint(1,3)
@@ -40,11 +49,39 @@ def procesar_pista(propuesta, numero):
         
     return {"Toro": toros, "Vaca": vacas}
 
+# Funcion auxiliar para devolver un string en formato json
+def crear_json(tipo, contenido):
+    diccionario = {"tipo": tipo, "contenido": contenido}
+    return json.dumps(diccionario)
+
+# Funcion auxiliar para enviar mensajes al cliente
+def enviar(msg):
+    mensaje = msg.encode(FORMATO)
+    longitud_mensaje = len(mensaje)
+    longitud_envio = str(longitud_mensaje).encode(FORMATO)
+    longitud_envio += b' ' * (HEADER - len(longitud_envio))
+    cliente_socket.send(longitud_envio)
+    cliente_socket.send(mensaje)
+
+# Funcion auxiliar para recibir mensajes del cliente
+def recibir():
+    mensaje = {}
+
+    longitud_mensaje = cliente_socket.recv(HEADER).decode(FORMATO)
+    if longitud_mensaje:
+        longitud_mensaje = int(longitud_mensaje)
+        mensaje = json.loads(cliente_socket.recv(longitud_mensaje).decode(FORMATO))
+    
+    return mensaje
+
+
 # Crear socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor_socket:
-    servidor_socket.bind(('localhost', 12345))
+    servidor_socket.bind(ADDR)
+    print("El servidor se esta iniciando...")
+
     servidor_socket.listen(1)
-    print("Servidor escuchando en localhost:12345")
+    print(f"Servidor escuchando en {SERVER}:{PUERTO}")
 
     # Esperar conexión
     cliente_socket, direccion_cliente = servidor_socket.accept()
@@ -54,27 +91,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor_socket:
     numero_secreto = generar_numero_secreto()
 
     # Enviar mensaje inicial
-    mensaje_inicial = {"tipo": "estado", "contenido": "JUEGO-CREADO"}
-    cliente_socket.send(json.dumps(mensaje_inicial).encode())
+    mensaje_inicial = crear_json("estado", JUEGO_LISTO)
+    enviar(mensaje_inicial)
 
     while True:
         # Recibir mensaje del cliente
-        mensaje_recibido = cliente_socket.recv(1024).decode()
-        if not mensaje_recibido:
-            break
-        
-        mensaje = json.loads(mensaje_recibido)
-        
+        mensaje = recibir()
+
         if mensaje["tipo"] == "proposicion":
             digitos_propuestos = mensaje["contenido"]
             
-            pista = procesar_pista(digitos_propuestos, numero_secreto)
-            
             if digitos_propuestos == numero_secreto:
-                respuesta = {"tipo": "estado", "contenido": "GANASTE"}
+                respuesta = crear_json("estado", JUEGO_GANADO)
             else:
-                respuesta = {"tipo": "pista", "pistas": pista}
+                pista = procesar_pista(digitos_propuestos, numero_secreto)
+                respuesta = crear_json("pista", pista)
             
-            cliente_socket.send(json.dumps(respuesta).encode())
+            enviar(respuesta)
+        elif mensaje["tipo"] == "estado":
+            if mensaje["contenido"] == DESCONECTAR_MENSAJE:
+                break
 
 print("Conexión finalizada")
